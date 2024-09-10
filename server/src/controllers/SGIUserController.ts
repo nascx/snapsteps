ec5f3e28475e3da6d83321136e842d702f2f8999
Wesley Silva do Nascimento <wnascimento@MKMONDIAL.local>
2024-09-10 09:34:23 -0400

    bug:não ler o titulo do arquivo

--- server/src/controllers/SGIUserController.ts
+++ server/src/controllers/SGIUserController.ts
@@ -29,7 +29,7 @@ export class SGIUserController {
             // pegando apenas a primeira página
             const page1buffer = await document.getPage(1)
             // definindo o caminho aonde a imagem será salva
-            const imagePath: string = path.join(__dirname, `../../_quality/${imageName}.bmp`)
+            const imagePath: string = path.join(__dirname, `../_quality/${imageName}.bmp`)
             // salvando a imagem no disco rígido
             await fs.writeFileSync(imagePath, page1buffer);
             // retornando o caminho da imagem
@@ -88,12 +88,12 @@ export class SGIUserController {
 
             // transformar o pdf em imagem
             // definindo caminho do arquivo
-            const filePath = path.resolve(__dirname, `../../_quality/${filename}`)
+            const filePath = path.resolve(__dirname, `../_quality/${filename}`)
 
             await this.convertPdf2Image(filePath, code)
             // obtendo o título do arquivo
             // definindo o caminho aonde a imagem será salva
-            const imagePath = path.resolve(__dirname, `../../_quality/${code}.bmp`)
+            const imagePath = path.resolve(__dirname, `../_quality/${code}.bmp`)
 
             const title = await this.getTitle(imagePath)
 
