a53a2d1c35455dcfb4f591ee3124c340d0ee550e
Wesley Silva do Nascimento <wnascimento@MKMONDIAL.local>
2024-09-30 10:55:23 -0400

    teste de alteração de banco de dados

--- client/src/urlApi.ts
+++ client/src/urlApi.ts
@@ -1,2 +1,2 @@
-export const urlAPi = 'http://10.12.100.156:4322'
-export const host = 'http://10.12.100.156:5433'
+export const urlAPi = 'http://10.12.100.156:8200'
+export const host = 'http://localhost:3000'
