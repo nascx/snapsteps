0f43ccc7899c1b5f73c26ace4e98671cc016dda1
Wesley Silva do Nascimento <wnascimento@MKMONDIAL.local>
2024-10-16 13:46:04 -0400

    nova atualização

--- server/src/config/db.ts
+++ server/src/config/db.ts
@@ -5,8 +5,8 @@ import { config } from 'dotenv'
 config()
 
 export const db = mysql.createPool({
-    host: process.env.DB_HOST || "10.12.100.156",
-    user: process.env.DB_USER || "root",
-    database: process.env.DB_DATABASE || "Helpdesk4202",
-    password: process.env.DB_PASSWORD || "snapsteps"
+    host: process.env.DB_HOST || '10.12.100.156',
+    user: process.env.DB_USER || 'root',
+    database: process.env.DB_DATABASE || "snapsteps",
+    password: process.env.DB_PASSWORD || "Helpdesk4202"
 })
